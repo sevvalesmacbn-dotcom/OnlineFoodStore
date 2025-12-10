ca2be673ce8405b844cb6757e82c10806897158f
Sevval Esma <Sevvalesma2006@gmail.com>
2025-12-11 00:29:51 +0300

    Customer model sınıfı oluşturuldu; phone ve address alanları eklendi, User sınıfından kalıtım sağlandı

--- src/main/java/com/onlinefoodstore/model/Customer.java
+++ src/main/java/com/onlinefoodstore/model/Customer.java
@@ -1,5 +1,23 @@
 package com.onlinefoodstore.model;
 
-public class Customer {
+public class Customer extends User {
+    private String phone;
+    private String address;
+
+    public Customer(){
+        super();
+    }
+    public Customer(String name, String username, int password, String role ,String phone ,String address){
+        super(name,username,password,role);
+        this.phone=phone;
+        this.address=address;
+    }
+    public String getPhone(){ return phone;}
+    public void setPhone(String phone){ this.phone=phone;}
+
+    public String getAddress(){ return address;}
+    public void setAddress(String address){ this.address=address;}
+
+
 
 }
