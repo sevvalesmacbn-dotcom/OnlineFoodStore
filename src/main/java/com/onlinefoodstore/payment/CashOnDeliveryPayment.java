35c22d2366ed767bbdc402735213e33bb5ac652f
Sevval Esma <Sevvalesma2006@gmail.com>
2025-12-26 17:36:30 +0300

    git add . git commit -m "Kapıda ödeme (Cash on Delivery) özelliği eklendi "

--- src/main/java/com/onlinefoodstore/payment/CashOnDeliveryPayment.java
+++ src/main/java/com/onlinefoodstore/payment/CashOnDeliveryPayment.java
@@ -1,4 +1,13 @@
 package com.onlinefoodstore.payment;
 
-public class CashOnDeliveryPayment {
+public class CashOnDeliveryPayment implements IPaymentMethod{
+    @Override
+    public void pay(double amount){
+        System.out.println("━━━━━━━━━━━━━━━━━━━━━━━━━━━━");
+        System.out.println("\uD83D\uDCB5 Kapıda Ödeme");
+        System.out.println("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━");
+        System.out.println("Tutar: "+String.format("%.2f TL",amount));
+        System.out.println("✅ Kapıda ödeme secildi.Ödemeyi kurye geldiğinde yapabilirsiniz.");
+        System.out.println("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━");
+    }
 }
