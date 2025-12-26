364fea7e3c561b83cd5a0c47af4786c783e40dd7
Sevval Esma <Sevvalesma2006@gmail.com>
2025-12-26 17:44:56 +0300

    Kredi kartı ile ödeme (Credit Card Payment) eklendi

--- src/main/java/com/onlinefoodstore/payment/CreditCardPayment.java
+++ src/main/java/com/onlinefoodstore/payment/CreditCardPayment.java
@@ -1,4 +1,13 @@
 package com.onlinefoodstore.payment;
 
-public class CreditCardPayment {
+public class CreditCardPayment implements IPaymentMethod{
+    @Override
+    public void pay(double amount) {
+        System.out.println("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━");
+        System.out.println("\uD83D\uDCB3 Kredi Kartı ile Ödeme");
+        System.out.println("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━");
+        System.out.println("Tutar: "+String.format("%.2f TL",amount));
+        System.out.println("✅ Kredi kartı ödemesi başarıyla gerçekleştirildi! ");
+        System.out.println("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━");
+    }
 }
