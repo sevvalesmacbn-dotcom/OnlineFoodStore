aa28c10c4de867b52f44d0a2e6ce22681f55421a
Sevval Esma <Sevvalesma2006@gmail.com>
2025-12-28 21:47:34 +0300

    DBConnection sınıfı ile veritabanı bağlantısı sağlandı

--- src/main/java/com/onlinefoodstore/dao/connection/DBConnection.java
+++ src/main/java/com/onlinefoodstore/dao/connection/DBConnection.java
@@ -1,5 +1,22 @@
 package com.onlinefoodstore.dao.connection;
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 public class DBConnection {
-
+    private static final String URL = "jdbc:mysql://localhost:3306/onlinefoodstore?useSSL=false&serverTimezone=UTC";
+    private static final String USER = "root";
+    private static final String PASSWORD = "123456";
+    public static Connection getConnection(){
+       try {
+           Class.forName("com.mysql.cj.jdbc.Driver");
+           return DriverManager.getConnection(URL, USER, PASSWORD);
+       } catch (ClassNotFoundException e) {
+           System.out.println("❌ MySQL JDBC Driver bulunamadı!");
+           e.printStackTrace();
+       }catch (SQLException e){
+           System.out.println("❌ Veritabanı bağlantısı başarısız!");
+           e.printStackTrace();
+       }
+       return null;
+    }
 }
