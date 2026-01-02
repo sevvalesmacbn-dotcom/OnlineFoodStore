46fc993f9a86a3d72184816a033b4d1c05dfa50f
Sevval Esma <Sevvalesma2006@gmail.com>
2026-01-02 20:50:54 +0300

    DBConnection bağlantı hatası giderildi.

--- src/main/java/com/onlinefoodstore/dao/connection/DBConnection.java
+++ src/main/java/com/onlinefoodstore/dao/connection/DBConnection.java
@@ -3,7 +3,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 public class DBConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/onlinefoodstore?useSSL=false&serverTimezone=UTC";
+    private static final String URL = "jdbc:mysql://localhost:3306/onlinefoodstore?allowPublicKeyRetrieval=true&useSSL=false";
     private static final String USER = "root";
     private static final String PASSWORD = "123456";
     public static Connection getConnection(){
